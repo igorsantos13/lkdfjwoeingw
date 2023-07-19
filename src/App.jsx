b9f42f1dafae975629db4332865d3db3d2ab1957
Igor Santos <igorsantos9513@outlook.com>
2023-07-19 19:26:34 -0300

    failed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <h4>pomodoro</h4>
+
+        <div className="timer">
+          <h1>00:00:00</h1>
+
+          <button>start</button>
+          <button>reset</button>
+        </div>
+      </div>
     </>
   )
 }
