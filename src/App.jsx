e3483c8f3242146a537341305443b8af508beb57
Igor Santos <igorsantos9513@outlook.com>
2023-07-20 19:00:01 -0300

    todo list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  //for todos
+  const [task, setTask] = useState('')
+  const [taskList, setTaskList] = useState([])
 
+  //for pomodoro
+  const [minutes, setMinutes] = useState(25)
+  const [seconds, setSeconds] = useState(11)
+  const [intervalId, setIntervalId] = useState(null);
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setTaskList((previousTasks)=> [...previousTasks, task])
+    setTask('')
+    
+  }
+
+  function handleStartTimer() {
+    if (!intervalId) {
+      const id = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+      setIntervalId(id);
+    }
+  }
+
+  function stopTimer(){
+    clearInterval(intervalId);
+    setIntervalId(null);
+  }
+
+  useEffect(() => {
+    if (seconds === 0) {
+      stopTimer();
+    }
+  }, [seconds]);
+  
   return (
     <>
+
+    <div>
+      <h1>TODO</h1>
+      <form action="">
+        <input type="text" onChange={(e)=>setTask(e.target.value)} value={task}/>
+        <button onClick={(event)=>handleClick(event)}>add task</button>
+
+        <ul>
+          {taskList?.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+
+      </form>
+    </div>
+
       <div>
         <h4>pomodoro</h4>
 
         <div className="timer">
-          <h1>00:00:00</h1>
+          <h1>00:{seconds < 10 ? `0${seconds}`: seconds}</h1>
 
-          <button>start</button>
+          <button onClick={handleStartTimer}>start</button>
           <button>reset</button>
         </div>
       </div>
